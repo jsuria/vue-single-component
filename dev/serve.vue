862153e0a8971f3c34d7a6ee9ce573b52496e6cb
Miko Suria <jose.suria@venn.bio>
2022-04-13 09:48:45 +0800

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueSingleComponent from '@/vue-single-component.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueSingleComponent
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-single-component />
+  </div>
+</template>
